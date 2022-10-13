fb0815e3c9eaa436b1eaa5713648552747ed947f
hyunyonglee <hyunyong.rhee@gmail.com>
2022-10-13 11:38:23 +0900

    revise

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ from tenpy.models.lattice import Site, Chain, Square
 from tenpy.models.model import CouplingModel, NearestNeighborModel, MPOModel, CouplingMPOModel
 from tenpy.linalg import np_conserved as npc
 from tenpy.tools.params import Config
-from tenpy.networks.site import BosonSite  # if you want to use the predefined site
+from tenpy.networks.site import SpinHalfFermionSite  # if you want to use the predefined site
 import matplotlib.pyplot as plt
 __all__ = ['DIPOLAR_FERMI_HUBBARD']
 
@@ -22,45 +22,52 @@ class DIPOLAR_FERMI_HUBBARD(CouplingModel,MPOModel):
         tp = model_params.get('tp', 0.)
         h = model_params.get('h', 0.)
         U = model_params.get('U', 0.)
-        Ut = model_params.get('Ut', 0.)
         mu = model_params.get('mu', 0.)
         bc_MPS = model_params.get('bc_MPS', 'infinite')
         bc = model_params.get('bc', 'periodic')
         QN = model_params.get('QN', 'N')
+        QS = model_params.get('QS', 'Sz')
 
-        site = SpinHalfFermionSite( cons_N='N', cons_Sz='Sz', filling=1.0 )
-        site.multiply_operators(['B','B'])
-        site.multiply_operators(['Bd','Bd'])
-
-        # MPS boundary condition
-        # if bc_MPS == 'finite' and bc == 'periodic':
-        #     order = 'folded'
-        # else:
+        site = SpinHalfFermionSite( cons_N=QN, cons_Sz=QS, filling=1.0 )
+        site.multiply_operators(['Cu','Cd'])
+        site.multiply_operators(['Cd','Cu'])
+        site.multiply_operators(['Cdd','Cdu'])
+        site.multiply_operators(['Cdu','Cdd'])
         
-        order = 'default'
+        # MPS boundary condition
+        if bc_MPS == 'finite' and bc == 'periodic':
+            order = 'folded'
+        else:
+        	order = 'default'
         
         lat = Chain( L=L, site=site, bc=bc, bc_MPS=bc_MPS, order=order )
         CouplingModel.__init__(self, lat)
 
         # 2-site hopping
-        self.add_coupling( -h, 0, 'B', 0, 'Bd', 1, plus_hc=True)
+        self.add_coupling( -h, 0, 'Cdu', 0, 'Cu', 1, plus_hc=True)
+        self.add_coupling( -h, 0, 'Cdd', 0, 'Cd', 1, plus_hc=True)
         
         # 3-site hopping
-        self.add_multi_coupling( -t, [('Bd', 0, 0), ('B B', 1, 0), ('Bd', 2, 0)])
-        self.add_multi_coupling( -t, [('B', 0, 0), ('Bd Bd', 1, 0), ('B', 2, 0)])
+        self.add_multi_coupling( -t, [('Cdu', 0, 0), ('Cu Cd', 1, 0), ('Cdd', 2, 0)])
+        self.add_multi_coupling( -t, [('Cdd', 0, 0), ('Cd Cu', 1, 0), ('Cdu', 2, 0)])
+        
+        self.add_multi_coupling( -t, [('Cd', 2, 0), ('Cdd Cdu', 1, 0), ('Cu', 0, 0)])
+        self.add_multi_coupling( -t, [('Cu', 2, 0), ('Cdu Cdd', 1, 0), ('Cd', 0, 0)])
+        
 
         # 4-site hopping
-        self.add_multi_coupling( -tp, [('Bd', 0, 0), ('B', 1, 0), ('B', 2, 0), ('Bd', 3, 0)])
-        self.add_multi_coupling( -tp, [('B', 0, 0), ('Bd', 1, 0), ('Bd', 2, 0), ('B', 3, 0)])
+        self.add_multi_coupling( -tp, [('Cdu', 0, 0), ('Cu', 1, 0), ('Cd', 2, 0), ('Cdd', 3, 0)])
+        self.add_multi_coupling( -tp, [('Cdd', 0, 0), ('Cd', 1, 0), ('Cu', 2, 0), ('Cdu', 3, 0)])
 
-        # NN Hubbard Interaction
-        self.add_coupling( Ut, 0, 'N', 0, 'N', 1)
+        self.add_multi_coupling( -tp, [('Cd', 3, 0), ('Cdd', 2, 0), ('Cdu', 1, 0), ('Cu', 0, 0)])
+        self.add_multi_coupling( -tp, [('Cu', 3, 0), ('Cdu', 2, 0), ('Cdd', 1, 0), ('Cd', 0, 0)])
         
+
         # Onsite Hubbard Interaction
-        self.add_onsite( U/2., 0, 'NN')
+        self.add_onsite( U, 0, 'NuNd')
 
         # Chemical potential
-        self.add_onsite( -( mu + U/2. ), 0, 'N')
+        self.add_onsite( -mu, 0, 'Ntot')
 
         
         MPOModel.__init__(self, lat, self.calc_H_MPO())
--- run_model.py
+++ run_model.py
@@ -39,19 +39,18 @@ L = int(sys.argv[1])
 t = float(sys.argv[2])
 tp = float(sys.argv[3])
 U = float(sys.argv[4])
-Ut = float(sys.argv[5])
-mu = float(sys.argv[6])
-Ncut = int(sys.argv[7])
-CHI = int(sys.argv[8])
-RM = sys.argv[9]
-QN = sys.argv[10]
-PATH = sys.argv[11]
-BC_MPS = sys.argv[12]
-BC = sys.argv[13]
-IS = sys.argv[14]
-TOL = float(sys.argv[15])
-h = float(sys.argv[16])
-EXC = sys.argv[17]
+mu = float(sys.argv[5])
+CHI = int(sys.argv[6])
+RM = sys.argv[7]
+QN = sys.argv[8]
+QS = sys.argv[9]
+PATH = sys.argv[10]
+BC_MPS = sys.argv[11]
+BC = sys.argv[12]
+IS = sys.argv[13]
+TOL = float(sys.argv[14])
+h = float(sys.argv[15])
+EXC = sys.argv[16]
 
 model_params = {
     "L": L,
@@ -59,111 +58,22 @@ model_params = {
     "tp": tp,
     "h": h,
     "U": U,
-    "Ut": Ut,
     "mu": mu,
-    "Ncut": Ncut,
     "bc_MPS": BC_MPS,
     "bc": BC,
-    "QN": QN
+    "QN": QN,
+    "QS": QS
 }
 
 print("\n\n\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-M = model.DIPOLAR_BOSE_HUBBARD(model_params)
+M = model.DIPOLAR_FERMI_HUBBARD(model_params)
 
 # initial state
-if IS == '1-1p':
-    product_state = ['1'] * M.lat.N_sites
-    product_state[int(L/2)] = '2'
+if IS == 'up':
+    product_state = ['up'] * M.lat.N_sites
+elif IS == 'up-down':
+    product_state = ['up','down'] * int(M.lat.N_sites/2)
 
-elif IS == '1-2p':
-    product_state = ['1'] * M.lat.N_sites
-    product_state[int(L/2)] = '2'
-    product_state[int(L/2)+1] = '2'
-    
-elif IS == '1-1h':
-    product_state = ['1'] * M.lat.N_sites
-    product_state[int(L/2)] = '0'
-    
-elif IS == '1-2h':
-    product_state = ['1'] * M.lat.N_sites
-    product_state[int(L/2)] = '0'
-    product_state[int(L/2)+1] = '0'
-
-elif IS == 'half':
-    product_state = ['1','0'] * int(M.lat.N_sites/2)
-    
-elif IS == 'half-1p':
-    product_state = ['1','0'] * int(M.lat.N_sites/2)
-    product_state[int(M.lat.N_sites/2)+1] = '1'
-
-elif IS == 'half-1h':
-    product_state = ['1','0'] * int(M.lat.N_sites/2)
-    product_state[int(M.lat.N_sites/2)] = '0'
-
-elif IS == '0-1half':
-    product_state = ['1','1','0','0'] * int(M.lat.N_sites/4)
-    
-elif IS == '1-1half':
-    product_state = ['2','2','1','1'] * int(M.lat.N_sites/4)
-    
-elif IS == '1-1half+1':
-    product_state = ['2','2','1','1'] * int(M.lat.N_sites/4)
-    product_state[2] = '2'
-    
-elif IS == '1-1half-1':
-    product_state = ['2','2','1','1'] * int(M.lat.N_sites/4)
-    product_state[1] = '1'
-    
-elif IS == '1.5+1':
-    product_state = ['1','2'] * int(M.lat.N_sites/2)
-    product_state[0] = '2'
-    
-elif IS == '1.5-1':
-    product_state = ['1','2'] * int(M.lat.N_sites/2)
-    product_state[1] = '1'
-    
-elif IS == '2+1':
-    product_state = ['2'] * M.lat.N_sites
-    product_state[int(M.lat.N_sites/2)] = '3'
-    
-elif IS == '2-1':
-    product_state = ['2'] * M.lat.N_sites
-    product_state[int(M.lat.N_sites/2)] = '1'
-    
-elif IS == '2-1half':
-    product_state = ['3','3','2','2'] * int(M.lat.N_sites/4)
-    
-elif IS == '3-1half':
-    product_state = ['4','4','3','3'] * int(M.lat.N_sites/4)
-    
-elif any( IS == frac for frac in ['0.1','0.2','0.3','0.4','0.5','0.6','0.7','0.8','0.9',
-    '1.1','1.2','1.3','1.4','1.5','1.6','1.7','1.8','1.9', 
-    '2.1','2.2','2.3','2.4','2.5','2.6','2.7','2.8','2.9',
-    '3.1','3.2','3.3','3.4','3.5','3.6','3.7','3.8','3.9','4.5','5.5'] ):
-    
-    n = float(IS)
-    dn = np.remainder(n,1)
-
-    product_state = [str(int(np.floor(n)))] * M.lat.N_sites
-    Ls = round(dn*M.lat.N_sites)
-
-    a = 0
-    I = 0
-    for i in range(M.lat.N_sites):
-
-        s = 2*i
-        if s < M.lat.N_sites:
-            a = 0    
-        else:
-            a = M.lat.N_sites-1
-    
-        product_state[s-a] = str(int(product_state[s-a])+1)
-        I = I+1
-        if I==Ls:
-            break
-
-else:
-    product_state = [IS] * M.lat.N_sites
     
 psi = MPS.from_product_state(M.lat.mps_sites(), product_state, bc=M.lat.bc_MPS)
 
@@ -220,8 +130,10 @@ ensure_dir(PATH + "mps/")
 # ground state
 eng = dmrg.TwoSiteDMRGEngine(psi, M, dmrg_params)
 E, psi = eng.run()  # equivalent to dmrg.run() up to the return parameters.
-N = psi.expectation_value("N")
-B = np.abs( psi.expectation_value("B") )
+Nu = psi.expectation_value("Nu")
+Nd = psi.expectation_value("Nd")
+Cu = np.abs( psi.expectation_value("Cu") )
+Cd = np.abs( psi.expectation_value("Cd") )
 EE = psi.entanglement_entropy()
 ES = psi.entanglement_spectrum()
 
@@ -236,94 +148,112 @@ else:
     xi = psi.correlation_length()
     I0 = 0
     R_CORR = 100
-    
-# measuring exciton condensation
-hs = []
-if BC_MPS == 'finite' and BC == 'periodic':
-    for i in range(0,int(L/2-1)): 
-        I = 2*i
-        hs.append( np.abs( psi.expectation_value_term([('Bd',I+2),('B',I)]) ) )
-    hs.append( np.abs( psi.expectation_value_term([('Bd',L-1),('B',L-2 )]) ) )
-    for i in range(0,int(L/2-1)):
-        I = L-1 - 2*i
-        hs.append( np.abs( psi.expectation_value_term([('Bd',I-2),('B',I)]) ) )
-    hs.append( np.abs( psi.expectation_value_term([('Bd',0),('B',1)]) ) )
 
-else:
-    for i in range(0,R): 
-        hs.append( np.abs( psi.expectation_value_term([('Bd',i+1),('B',i)]) ) )
+# measuring exciton condensation
+hus = []
+hds = []
+for i in range(0,R): 
+    hus.append( np.abs( psi.expectation_value_term([('Cdu',i+1),('Cu',i)]) ) )
+    hds.append( np.abs( psi.expectation_value_term([('Cdd',i+1),('Cd',i)]) ) )
 #
 
-
 # measuring correlation functions
-cor_bb = []
+cor_cucu = []
+cor_cdcd = []
+cor_du = []
+cor_du_conn = []
 cor_dd = []
 cor_dd_conn = []
 for i in range(R_CORR):
 
-    cor = psi.expectation_value_term([('Bd',I0),('B',I0+1),('B',I0+2+i),('Bd',I0+3+i)])
-    bb1 = psi.expectation_value_term([('Bd',I0),('B',I0+1)])
-    bb2 = psi.expectation_value_term([('B',I0+2+i),('Bd',I0+3+i)])
+    cor = psi.expectation_value_term([('Cdu',I0+1),('Cu',I0),('Cdu',I0+2+i),('Cu',I0+3+i)])
+    cc1 = psi.expectation_value_term([('Cdu',I0+1),('Cu',I0)])
+    cc2 = psi.expectation_value_term([('Cdu',I0+2+i),('Cu',I0+3+i)])
     
-    cor_bb.append( np.abs( psi.expectation_value_term([('Bd',I0),('B',I0+1+i)]) ) )
+    cor_du.append( np.abs( cor ) )
+    cor_du_conn.append( np.abs( cor - cc1*cc2 ) )   
+
+    cor = psi.expectation_value_term([('Cdd',I0+1),('Cd',I0),('Cdd',I0+2+i),('Cd',I0+3+i)])
+    cc1 = psi.expectation_value_term([('Cdd',I0+1),('Cd',I0)])
+    cc2 = psi.expectation_value_term([('Cdd',I0+2+i),('Cd',I0+3+i)])
+    
     cor_dd.append( np.abs( cor ) )
-    cor_dd_conn.append( np.abs( cor - bb1*bb2 ) )   
-#
+    cor_dd_conn.append( np.abs( cor - cc1*cc2 ) )   
+
+    cor_cucu.append( np.abs( psi.expectation_value_term([('Cdu',I0),('Cu',I0+1+i)])))
+    cor_cdcd.append( np.abs( psi.expectation_value_term([('Cdd',I0),('Cd',I0+1+i)])))
 
 
-#
-if EXC == 'ON':
-    dmrg_params['orthogonal_to'] = [psi]
-    psi1 = psi.copy()
-    eng1 = dmrg.TwoSiteDMRGEngine(psi1, M, dmrg_params)
-    E1, psi1 = eng1.run()  # equivalent to dmrg.run() up to the return parameters.
-    gap = E1 - E
 
-    with open( PATH + 'mps/exc_t_%.2f_tp_%.2f_U%.2f_Ut%.2f_mu%.2f.pkl' % (t,tp,U,Ut,mu), 'wb') as f:
-        pickle.dump(psi1, f)
+file = open( PATH + "observables/energy.txt","a")
+file.write(repr(t) + " " + repr(tp) + " " + repr(U) + " " + repr(mu) + " " + repr(E) + " " + repr( np.mean(Nu) ) + " " + repr( np.mean(Nd) ) + " " + repr( np.mean(Cu) ) + " " + repr( np.mean(Cd) ) + " " + repr( np.mean(hus) ) + " " + repr( np.mean(hds) ) + " " + repr(xi) + " " + "\n")
+file.close()
 
-else:
-    gap = 0.
-#
+file = open( PATH + "observables/Nu.txt","a")
+file.write(repr(t) + " " + repr(tp) + " " + repr(U) + " " + repr(mu) + " " + "  ".join(map(str, Nu)) + " " + "\n")
+file.close()
 
+file = open( PATH + "observables/Nd.txt","a")
+file.write(repr(t) + " " + repr(tp) + " " + repr(U) + " " + repr(mu) + " " + "  ".join(map(str, Nd)) + " " + "\n")
+file.close()
 
-file1 = open( PATH + "observables/energy.txt","a")
-file1.write(repr(t) + " " + repr(tp) + " " + repr(U) + " " + repr(Ut) + " " + repr(mu) + " " + repr(E) + " " + repr( np.mean(N) ) + " " + repr( np.mean(B) ) + " " + repr( np.mean(hs) ) + " " + repr(xi) + " " + repr(gap) + " " + "\n")
+file = open( PATH + "observables/Cu.txt","a")
+file.write(repr(t) + " " + repr(tp) + " " + repr(U) + " " + repr(mu) + " " + "  ".join(map(str, Cu)) + " " + "\n")
+file.close()
 
-file2 = open( PATH + "observables/numbers.txt","a")
-file2.write(repr(t) + " " + repr(tp) + " " + repr(U) + " " + repr(Ut) + " " + repr(mu) + " " + "  ".join(map(str, N)) + " " + "\n")
+file = open( PATH + "observables/Cd.txt","a")
+file.write(repr(t) + " " + repr(tp) + " " + repr(U) + " " + repr(mu) + " " + "  ".join(map(str, Cd)) + " " + "\n")
+file.close()
 
-file3 = open( PATH + "observables/exciton_density.txt","a")
-file3.write(repr(t) + " " + repr(tp) + " " + repr(U) + " " + repr(Ut) + " " + repr(mu) + " " + "  ".join(map(str, hs)) + " " + "\n")
+file = open( PATH + "observables/Du.txt","a")
+file.write(repr(t) + " " + repr(tp) + " " + repr(U) + " " + repr(mu) + " " + "  ".join(map(str, hus)) + " " + "\n")
+file.close()
 
-file4 = open( PATH + "observables/condensation.txt","a")
-file4.write(repr(t) + " " + repr(tp) + " " + repr(U) + " " + repr(Ut) + " " + repr(mu) + " " + "  ".join(map(str, B)) + " " + "\n")
+file = open( PATH + "observables/Dd.txt","a")
+file.write(repr(t) + " " + repr(tp) + " " + repr(U) + " " + repr(mu) + " " + "  ".join(map(str, hds)) + " " + "\n")
+file.close()
 
-file5 = open( PATH + "observables/entanglement_entropy.txt","a")
-file5.write(repr(t) + " " + repr(tp) + " " + repr(U) + " " + repr(Ut) + " " + repr(mu) + " " + "  ".join(map(str, EE)) + " " + "\n")
+file = open( PATH + "observables/entanglement_entropy.txt","a")
+file.write(repr(t) + " " + repr(tp) + " " + repr(U) + " " + repr(mu) + " " + "  ".join(map(str, EE)) + " " + "\n")
+file.close()
 
-file6 = open( PATH + "observables/corr_bb.txt","a")
-file6.write(repr(t) + " " + repr(tp) + " " + repr(U) + " " + repr(Ut) + " " + repr(mu) + " " + "  ".join(map(str, cor_bb)) + " " + "\n")
+file = open( PATH + "observables/corr_cucu.txt","a")
+file.write(repr(t) + " " + repr(tp) + " " + repr(U) + " " + repr(mu) + " " + "  ".join(map(str, cor_cucu)) + " " + "\n")
+file.close()
 
-file7 = open( PATH + "observables/corr_dd.txt","a")
-file7.write(repr(t) + " " + repr(tp) + " " + repr(U) + " " + repr(Ut) + " " + repr(mu) + " " + "  ".join(map(str, cor_dd)) + " " + "\n")
+file = open( PATH + "observables/corr_cdcd.txt","a")
+file.write(repr(t) + " " + repr(tp) + " " + repr(U) + " " + repr(mu) + " " + "  ".join(map(str, cor_cdcd)) + " " + "\n")
+file.close()
 
-file8 = open( PATH + "observables/corr_dd_conn.txt","a")
-file8.write(repr(t) + " " + repr(tp) + " " + repr(U) + " " + repr(Ut) + " " + repr(mu) + " " + "  ".join(map(str, cor_dd_conn)) + " " + "\n")
+file = open( PATH + "observables/corr_du.txt","a")
+file.write(repr(t) + " " + repr(tp) + " " + repr(U) + " " + repr(mu) + " " + "  ".join(map(str, cor_du)) + " " + "\n")
+file.close()
 
-file_ES = open( PATH + "entanglement/es_t_%.3f_tp_%.3f_U_%.2f_Ut_%.2f_mu_%.2f.txt" % (t,tp,U,Ut,mu),"a")
+file = open( PATH + "observables/corr_dd.txt","a")
+file.write(repr(t) + " " + repr(tp) + " " + repr(U) + " " + repr(mu) + " " + "  ".join(map(str, cor_dd)) + " " + "\n")
+file.close()
+
+file = open( PATH + "observables/corr_du_conn.txt","a")
+file.write(repr(t) + " " + repr(tp) + " " + repr(U) + " " + repr(mu) + " " + "  ".join(map(str, cor_du_conn)) + " " + "\n")
+file.close()
+
+file = open( PATH + "observables/corr_dd_conn.txt","a")
+file.write(repr(t) + " " + repr(tp) + " " + repr(U) + " " + repr(mu) + " " + "  ".join(map(str, cor_dd_conn)) + " " + "\n")
+file.close()
+
+file_ES = open( PATH + "entanglement/es_t_%.3f_tp_%.3f_U_%.2f_mu_%.2f.txt" % (t,tp,U,mu),"a")
 for i in range(0,R):
     file_ES.write("  ".join(map(str, ES[i])) + " " + "\n")
-file_EE = open( PATH + "entanglement/ee_t_%.3f_tp_%.3f_U_%.2f_Ut_%.2f_mu_%.2f.txt" % (t,tp,U,Ut,mu),"a")
+file_EE = open( PATH + "entanglement/ee_t_%.3f_tp_%.3f_U_%.2f_mu_%.2f.txt" % (t,tp,U,mu),"a")
 file_EE.write("  ".join(map(str, EE)) + " " + "\n")
 
-file_STAT = open( PATH + "logs/stat_t_%.3f_tp_%.3f_U_%.2f_Ut_%.2f_mu_%.2f.txt" % (t,tp,U,Ut,mu),"a")
+file_STAT = open( PATH + "logs/stat_t_%.3f_tp_%.3f_U_%.2f_mu_%.2f.txt" % (t,tp,U,mu),"a")
 file_STAT.write("  ".join(map(str,eng.sweep_stats['E'])) + " " + "\n")
 file_STAT.write("  ".join(map(str,eng.sweep_stats['S'])) + " " + "\n")
 file_STAT.write("  ".join(map(str,eng.sweep_stats['max_trunc_err'])) + " " + "\n")
 file_STAT.write("  ".join(map(str,eng.sweep_stats['norm_err'])) + " " + "\n")
 
-with open( PATH + 'mps/gs_t_%.2f_tp_%.2f_U%.2f_Ut%.2f_mu%.2f.pkl' % (t,tp,U,Ut,mu), 'wb') as f:
+with open( PATH + 'mps/gs_t_%.2f_tp_%.2f_U%.2f_mu%.2f.pkl' % (t,tp,U,mu), 'wb') as f:
     pickle.dump(psi, f)
 
 
