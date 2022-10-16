476aac4ab11c6d0a2dad36329dca9e5e63e1ba5d
hyunyonglee <hyunyong.rhee@gmail.com>
2022-10-16 14:36:43 +0900

    revise

--- run_model.py
+++ run_model.py
@@ -82,7 +82,7 @@ elif any( IS == frac for frac in ['0.1','0.2','0.3','0.4','0.5','0.6','0.7','0.8
     n = float(IS)
     Ls = round(n*L/2)
     product_state = ['up','down'] * Ls + ['empty'] * (L-2*Ls)
-    product_state = random.shuffle(product_state)
+    random.shuffle(product_state)
     # Ls = round(n*L)
     # product_state = ['full'] * Ls + ['empty'] * (L-Ls)
     
