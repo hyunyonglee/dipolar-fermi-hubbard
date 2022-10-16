11cdb862f8d9ef25589f9def768880f2720eb76e
hyunyonglee <hyunyong.rhee@gmail.com>
2022-10-16 12:42:02 +0900

    revise

--- run_model.py
+++ run_model.py
@@ -77,10 +77,10 @@ elif IS == 'empty-full':
 elif any( IS == frac for frac in ['0.1','0.2','0.3','0.4','0.5','0.6','0.7','0.8','0.9','1.0'] ):
     
     n = float(IS)
-    # Ls = round(n*L/2)
-    # product_state = ['up','down'] * Ls + ['empty'] * (L-2*Ls)
-    Ls = round(n*L)
-    product_state = ['full'] * Ls + ['empty'] * (L-Ls)
+    Ls = round(n*L/2)
+    product_state = ['up','down'] * Ls + ['empty'] * (L-2*Ls)
+    # Ls = round(n*L)
+    # product_state = ['full'] * Ls + ['empty'] * (L-Ls)
     
 psi = MPS.from_product_state(M.lat.mps_sites(), product_state, bc=M.lat.bc_MPS)
 
